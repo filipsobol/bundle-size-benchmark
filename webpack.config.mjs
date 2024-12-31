5219e3f8cc6e830a5a5e67cf74c99bf0d785fcec
SukkaW <isukkaw@gmail.com>
2024-12-31 12:25:34 +0800

    chore: enable process.env.NODE_ENV handling for webpack

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,22 +1,33 @@
+import { DefinePlugin } from 'webpack';
 import Terser from 'terser-webpack-plugin';
+import { plugins } from 'chart.js';
 
 export default {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'webpack.js',
     clean: false
   },
+  plugins: [
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'import.meta.env.DEV': (false).toString(),
+      'import.meta.env.PROD': (true).toString(),
+      'typeof window': JSON.stringify('object'),
+    })
+  ],
   optimization: {
     minimize: true,
     minimizer: [
-      new Terser( {
+      new Terser({
         minify: Terser.swcMinify,
         terserOptions: {
           format: {
             comments: false
           }
         }
-      } )
+      })
     ]
   }
 };
